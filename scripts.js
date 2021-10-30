7786f0d8580871ffdd9ce95b4552e6900c17a6de
TS93 <tschram93@yahoo.com>
2021-10-30 19:08:33 -0400

    progress bar of video player updates with time in video

--- scripts.js
+++ scripts.js
@@ -25,15 +25,28 @@ function handleRangeUpdate() {
     video[this.name] = this.value;
 }
 
+function handleProgress() {
+    const percent = (video.currentTime / video.duration) * 100;
+    progressBar.style.flexBasis = `${percent}%`;
+}
+
+function scrub(e) {
+    const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+    video.currentTime = scrubTime;
+}
+
 function skip() {
     // console.log('Skipping');
     video.currentTime += parseFloat(this.dataset.skip);
 
 }
 // Create Event Listeners
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', scrub);
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
 ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
 ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
 skipButtons.forEach(button => button.addEventListener('click', skip));
